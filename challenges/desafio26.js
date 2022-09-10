f1f22abcc62196a4ebe03afb3df5f949259bfedc
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-10 03:23:19 -0300

    feat: :sparkles: Criando a 26ª query
    Criando query que adiciona "contem sodio" nas tags dos produtos que pusiem persentual de sodio mior que 20 e menor que 40

---
+++ challenges/desafio26.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({ valoresNutricionais: {
+    $elemMatch: {
+        tipo: "sódio",
+        percentual: { $lt: 40, $gt: 20 },
+    } } },
+    { $push: { tags: "contém sódio" } });
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
